c558c97f810f2543dade57bde3820112cb6105af
Binis <binisbelev@gmail.com>
2020-07-01 14:22:34 +0300

    Embedded collections flow improved

--- src/main/java/net/binis/demo/codegen/CollectionsHandler.java
+++ src/main/java/net/binis/demo/codegen/CollectionsHandler.java
@@ -188,7 +188,7 @@ public class CollectionsHandler {
                     .addTypeParameter("T")
                     .setInterface(true);
             modifier.addMethod("and")
-                    .setType("T")
+                    .setType("EmbeddedCollection<EmbeddedModify<T>, Inner, T>")
                     .setBody(null);
 
             var modifierClass = new ClassOrInterfaceDeclaration(
@@ -204,13 +204,17 @@ public class CollectionsHandler {
                             .addStatement(new AssignExpr().setTarget(new NameExpr().setName("this.parent")).setValue(new NameExpr().setName("parent")))
                             .addStatement(new AssignExpr().setTarget(new NameExpr().setName("this.entity")).setValue(new NameExpr().setName("entity"))));
             modifierClass.addMethod("and", PUBLIC)
-                    .setType("T")
-                    .setBody(new BlockStmt().addStatement(new ReturnStmt().setExpression(new NameExpr().setName("parent"))));
+                    .setType("EmbeddedCollection<EmbeddedModify<T>, Inner, T>")
+                    .setBody(new BlockStmt().addStatement(new ReturnStmt().setExpression(new NameExpr().setName("(EmbeddedCollection) parent"))));
 
             spec.addMember(modifierClass);
             intf.addMember(modifier);
 
-            spec.findCompilationUnit().get().addImport("net.binis.demo.factory.CodeFactory");
+            intf.findCompilationUnit().get().addImport("net.binis.demo.collection.EmbeddedCollection");
+            spec.findCompilationUnit().ifPresent(u -> {
+                u.addImport("net.binis.demo.factory.CodeFactory");
+                u.addImport("net.binis.demo.collection.EmbeddedCollection");
+            });
             spec.addStaticInitializer();
         }
     }
--- src/main/java/net/binis/demo/collection/EmbeddedCollection.java
+++ src/main/java/net/binis/demo/collection/EmbeddedCollection.java
@@ -1,8 +1,8 @@
 package net.binis.demo.collection;
 
-public interface EmbeddedCodeList<M, T, R> {
+public interface EmbeddedCollection<M, T, R> {
 
-    EmbeddedCodeList<M, T, R> add(T value);
+    EmbeddedCollection<M, T, R> add(T value);
     M add();
     R and();
 
